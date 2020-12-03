c8f0b147fae018d2e3ffde29d2412ecdf3d6781e
Reuben Tan <RTJL@users.noreply.github.com>
2020-12-04 02:38:04 +0800

    Staging v0.1 (#2)
    * add route53, cloudfront, s3 buckets
    
    * update price class for cloudfront
    
    * update readme
    
    * update
    
    * add template tfvar file

--- main.tf
+++ main.tf
@@ -10,133 +10,155 @@ terraform {
   }
 }
 
-resource "aws_vpc" "this" {
-  cidr_block           = var.cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "${var.project}-vpc-${var.env}"
+locals {
+  website_bucket_name = "bus-frontend-app-${var.env}"
+  website_endpoint = trimprefix(aws_s3_bucket.website_bucket.website_endpoint, "https://")
+  api_origin_path = "/${var.env}"
+  api_origin_without_http = trimprefix(data.aws_ssm_parameter.api_gateway_endpoint.value, "https://")
+  api_gateway_domain_name = trimsuffix(local.api_origin_without_http, local.api_origin_path)
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = local.website_bucket_name
+  acl = "public-read"
+  policy = data.aws_iam_policy_document.website_policy.json
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
   }
 }
 
-resource "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project}/${var.env}/vpc/id"
-  description = "VPC ID"
+resource "aws_ssm_parameter" "website_bucket_name" {
+  name = "/${var.project}/${var.env}/s3/website_bucket_name"
+  description = "Website Bucket Name"
   type = "String"
-  value = aws_vpc.this.id
+  value = aws_s3_bucket.website_bucket.id
 
   tags = {
-    Name = "${var.project}-vpc-${var.env}"
+    Name = "${var.project}-${var.env}"
   }
 }
 
-resource "aws_subnet" "public" {
-  count = length(var.public_subnets)
+resource "aws_cloudfront_distribution" "website_distribution" {
+  origin {
+    custom_origin_config {
+      http_port = "80"
+      https_port = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+    domain_name = local.website_endpoint
+    origin_id = local.website_endpoint
+  }
 
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = element(concat(var.public_subnets, [""]), count.index)
-  availability_zone       = element(concat(var.azs, [""]), count.index)
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.project}-public-${count.index}-${var.env}"
+  origin {
+    domain_name = local.api_gateway_domain_name
+    origin_id = local.api_gateway_domain_name
+    origin_path = local.api_origin_path
+
+    custom_origin_config {
+      http_port = "80"
+      https_port = "443"
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled = true
+  default_root_object = "index.html"
+  price_class = "PriceClass_200"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = local.website_endpoint
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "api/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods  = ["GET", "HEAD"]
+    compress = true
+    default_ttl = 0
+    max_ttl = 0
+    min_ttl = 0
+    smooth_streaming = false
+    target_origin_id = local.api_gateway_domain_name
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      headers = ["Accept", "Authorization"]
+      cookies {
+        forward = "all"
+      }
+      query_string = true
+    }
+  }
+
+  aliases = [ var.www_domain_name ]
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_ssm_parameter.cert_arn.value
+    ssl_support_method = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
   }
 }
 
-resource "aws_ssm_parameter" "subnet__public_id" {
-  name = "/${var.project}/${var.env}/subnet/public/id"
-  description = "Public Subnet IDs"
-  type = "StringList"
-  value = join(",", aws_subnet.public.*.id)
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_ssm_parameter.host_zone_id.value
+  name    = var.www_domain_name
+  type    = "A"
 
-  tags = {
-    Name = "${var.project}-vpc-${var.env}"
+  alias {
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
+    evaluate_target_health = false
   }
 }
 
-resource "aws_subnet" "private" {
-  count = length(var.private_subnets)
-
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = element(concat(var.private_subnets, [""]), count.index)
-  availability_zone       = element(concat(var.azs, [""]), count.index)
-  tags = {
-    Name = "${var.project}-private-${count.index}-${var.env}"
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::${local.website_bucket_name}/*"
+    ]
   }
 }
 
-resource "aws_ssm_parameter" "subnet__private_id" {
-  name = "/${var.project}/${var.env}/subnet/private/id"
-  description = "Private Subnet IDs"
-  type = "StringList"
-  value = join(",", aws_subnet.private.*.id)
-
-  tags = {
-    Name = "${var.project}-vpc-${var.env}"
-  }
+data "aws_ssm_parameter" "cert_arn" {
+  name = "/bus-backend-infra/${var.env}/certManager/arn"
+  with_decryption = true
 }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${var.project}-rt-${var.env}"
-  }
-}
-resource "aws_route" "public_igw_route" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.this.id
-
-  timeouts {
-    create = "5m"
-  }
-}
-resource "aws_route_table_association" "public" {
-  count = length(var.public_subnets)
-
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public.id
-}
-resource "aws_internet_gateway" "this" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = "${var.project}-igw-${var.env}"
-  }
+data "aws_ssm_parameter" "api_gateway_endpoint" {
+  name = "/bus-backend-app/${var.env}/apiGateway/endpoint"
+  with_decryption = true
 }
 
-resource "aws_security_group" "default_public" {
-  name        = "${var.project}_default_public_sg-${var.env}"
-  description = "${var.project} default public SG"
-  vpc_id      = aws_vpc.this.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow http access"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.project}_public_sg-${var.env}"
-  }
-}
-
-resource "aws_ssm_parameter" "security_group_id" {
-  name = "/${var.project}/${var.env}/securityGroup/id"
-  description = "Security Group IDs"
-  type = "StringList"
-  value = join(",", [aws_security_group.default_public.id])
-
-  tags = {
-    Name = "${var.project}-vpc-${var.env}"
-  }
+data "aws_ssm_parameter" "host_zone_id" {
+  name = "/bus-backend-infra/${var.env}/route53/hostZoneId"
+  with_decryption = true
 }
---
+++ vars/template.tfvars
@@ -0,0 +1,8 @@
+project         = ""
+env             = ""
+region          = ""
+cidr_block      = ""
+public_subnets  = ["", ""]
+private_subnets = ["", ""]
+azs             = ["", ""]
+www_domain_name = ""
