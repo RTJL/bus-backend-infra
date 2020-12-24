e286b385b8bdda40c86c4ba166d1eaf107cacf80
Reuben Tan <reubentjl@gmail.com>
2020-12-24 18:29:40 +0800

    remove root module

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "website_bucket_name" {
-  description = "Static website bucket name"
-  value = aws_s3_bucket.website_bucket.id
-}
-
-output "website_bucket_arn" {
-  description = "Static website bucket ARN"
-  value = aws_s3_bucket.website_bucket.arn
-}
